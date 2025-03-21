3720987c926c9d1a508a05aa530b6ba892682b49
Jhonata <jhonatafontesluis23@gmail.com>
2025-03-20 22:11:30 -0300

    Criado default-cadastro e pages cadastro-component
    Signed-off-by: Jhonata <jhonatafontesluis23@gmail.com>

--- src/app/components/default-cadastro-layout/default-cadastro-layout.component.ts
+++ src/app/components/default-cadastro-layout/default-cadastro-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-cadastro-layout',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './default-cadastro-layout.component.scss'
 })
 export class DefaultCadastroLayoutComponent {
-
+  @Input() title: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secundaryBtnText: string = "";
 }
--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { DefaultCadastroLayoutComponent } from '../../components/default-cadastro-layout/default-cadastro-layout.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
   imports: [
-    DefaultCadastroLayoutComponent
+    DefaultCadastroLayoutComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
+cadastroForm!: FormGroup;
 
+  constructor(){
+    this.cadastroForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      cep: new FormControl('', Validators.required)
+    })
+  }
 }
